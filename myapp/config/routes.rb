31cdca767f8a8a614a58621058950a4bcff48f1b
Joel Myers <joeldmyers@gmail.com>
2017-06-19 22:30:34 -0400

    named routes

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/pages/about" => "pages#about"
+  get "/pages/about" => "pages#about", as: :about
 
-  get "/pages/contact" => "pages#contact"
+  get "/pages/contact" => "pages#contact", as: :contact
 
 end
